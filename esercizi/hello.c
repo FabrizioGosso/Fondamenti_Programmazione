b64a72565555d3a5d4b0a0bf70d325fbce7d273d
FabrizioGosso <fabrizio.gosso@yahoo.it>
2024-12-20 16:09:08 +0000

    update scanf

--- esercizi/hello.c
+++ esercizi/hello.c
@@ -4,6 +4,14 @@
 //il main restituisce un intero
 int main()  
 {
-    printf("HELLO WORLD!\nCIAO MONDO!\n\n\n"); // \n nuova righa a capo
+    printf("HELLO WORLD!\nCIAO MONDO!\n\n\n"); // \n nuova righa a capo   \t tab
+
+    char name[32]; // dichiaro una variabile da 32 caratteri
+
+    printf("Inserisci il tuo nome:  ");
+    scanf("%s", name); //legge l'input dell'utente
+
+    printf("Il tuo nome è: %s\n\n", name);
+
     return 0; // program is terminated successfully
 }
