920fb70ec917844cc24dae12a9266a3af91bb19c
FabrizioGosso <fabrizio.gosso@yahoo.it>
2024-12-20 15:44:33 +0000

    Nuovo esercizio

---
+++ esercizi/hello.c
@@ -0,0 +1,9 @@
+#include <stdio.h> //input output library
+
+//main function - the entry point
+//il main restituisce un intero
+int main()  
+{
+    printf("HELLO WORLD!\nCIAO MONDO!\n\n\n"); // \n nuova righa a capo
+    return 0; // program is terminated successfully
+}
