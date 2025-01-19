fa4b34e9d8dee27a0530cd0cb18d322a33d19db6
FabrizioGosso <fabrizio.gosso@yahoo.it>
2025-01-19 19:21:11 +0000

    esercizio2

---
+++ esercizi/es_lez2.c
@@ -0,0 +1,28 @@
+#include <stdio.h>;
+
+int main(){
+
+char nome[32];
+char cognome[32];
+
+// Inserimento credenziali + messaggio introduttivo
+printf("Inserisci il tuo nome: \n");
+scanf('%s', nome);
+
+printf("Inserisci il tuo cogonome: \n");
+scanf('%s', cognome);
+
+printf("Benvenuto, %s %s!\n", nome, cognome);
+
+// Inserimento dati 
+char categoria[3];
+float peso;
+
+printf("Inserisci la categoria del tuo dispositivo: \n");
+scanf('%s', categoria);
+
+printf("Inserisci il peso in Kg: \n");
+scanf('%f', &peso);
+
+
+}
