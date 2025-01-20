de0f04593f4719b94996b4b02d3b68d5dbb38ac8
FabrizioGosso <fabrizio.gosso@yahoo.it>
2025-01-20 10:45:57 +0000

    esercizio2 completo

--- esercizi/es_lez2.c
+++ esercizi/es_lez2.c
@@ -1,29 +1,63 @@
-#include <stdio.h>;
+#include <stdio.h>
 #include <string.h>
+
+// Macros
+#define NOME "Fabrizio"
+#define COGNOME "Gosso"
+#define R1 0.85
+#define R2 0.75
+#define R3 0.65
+#define R4 0.50
+#define R5 0.40
+
 
 int main(){
 
-char nome[32];
-char cognome[32];
-
-// Inserimento credenziali + messaggio introduttivo
-printf("Inserisci il tuo nome: \n");
-scanf('%s', nome);
-
-printf("Inserisci il tuo cogonome: \n");
-scanf('%s', cognome);
-
-printf("Benvenuto, %s %s!\n", nome, cognome);
-
-// Inserimento dati 
+// Dichiarazione variabili 
 char categoria[3];
-float peso;
+double peso, riclabile, scarto;
 
-printf("Inserisci la categoria del tuo dispositivo: \n");
-scanf('%s', categoria);
+// Messaggio e input dati
+printf("Benvenuto, %s %s!\n", NOME, COGNOME);
+
+printf("Inserisci la categoria RAEE del tuo dispositivo: \n");
+scanf("%2s", categoria);
 
 printf("Inserisci il peso in Kg: \n");
-scanf('%f', &peso);
+scanf("%lf", &peso);
 
+// Calcolo a seconda della categoria inserita ---- strcasecmp confronta le stringhe ignorando maiuscole e minuscole
+if (strcasecmp(categoria, "R1") == 0) {
+    riclabile = peso * R1;
+    } 
+else if (strcasecmp(categoria, "R2") == 0) {
+    riclabile = peso * R2;
+    }
+else if (strcasecmp(categoria, "R3") == 0) {
+    riclabile = peso * R3;
+    }
+else if (strcasecmp(categoria, "R4") == 0) {
+    riclabile = peso * R4;
+    }
+else if (strcasecmp(categoria, "R5") == 0) {
+    riclabile = peso * R5;
+    }
+else {
+    printf("Categoria non valida!\n");
+    return 1;  // Uscita con errore
+    }
+
+// Calcolo del materiale irrecuperabile
+scarto = peso - riclabile;
+
+// Stampa dei risultati
+printf("\nPeso totale: %.2f kg.\n", peso);
+printf("Recuperabile: %.2f kg.\n", riclabile);
+printf("Irrecuperabile: %.2f kg.\n", scarto);
+
+
+printf("Grazie per aver usato il nostro programma.\n\n");
+
+return 0;
 
 }
