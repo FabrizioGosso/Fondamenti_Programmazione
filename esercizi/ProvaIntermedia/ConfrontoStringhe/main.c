0eb3692803fd1e9b4972a226a599107a2a8a39b3
FabrizioGosso <fabrizio.gosso@yahoo.it>
2025-02-07 15:51:29 +0000

    agg

--- esercizi/ProvaIntermedia/ConfrontoStringhe/main.c
+++ esercizi/ProvaIntermedia/ConfrontoStringhe/main.c
@@ -2,8 +2,46 @@
 #include "functions.h"
 
 int main() {
-    char words[][20] = {"elephant", "42trees", "Anna", "oasis", "civic", "mango", "Galaxy"};
-    int size = sizeof(words) / sizeof(words[0]);
+    int choice;
+    char (*words)[20];
+    int size;
+
+    // Chiedi all'utente se vuole usare l'array predefinito o inserire uno nuovo
+    printf("Vuoi usare l'array predefinito o inserirne uno nuovo?\n");
+    printf("1. Usare l'array predefinito\n");
+    printf("2. Inserire un nuovo array\n");
+    printf("Scegli (1 o 2): ");
+    scanf("%d", &choice);
+
+    if (choice == 1) {
+        // Array predefinito
+        char defaultWords[][20] = {"elephant", "42trees", "Anna", "oasis", "civic", "mango", "Galaxy"};
+        size = sizeof(defaultWords) / sizeof(defaultWords[0]);
+        words = defaultWords; // Usa l'array predefinito
+    } else if (choice == 2) {
+        // Chiedi all'utente quanti elementi vuole inserire
+        printf("Quante stringhe vuoi inserire? ");
+        scanf("%d", &size);
+
+        // Creazione dinamica dell'array di stringhe
+        words = malloc(size * sizeof(*words));
+
+        // Controlla se la memoria è stata allocata correttamente
+        if (words == NULL) {
+            printf("Memoria insufficiente!\n");
+            return 1;
+        }
+
+        // Chiedi all'utente di inserire le stringhe
+        printf("Inserisci le %d stringhe:\n", size);
+        for (int i = 0; i < size; i++) {
+            printf("Stringa %d: ", i + 1);
+            scanf("%s", words[i]);
+        }
+    } else {
+        printf("Scelta non valida. Uscita...\n");
+        return 1;
+    }
 
     printf("Numero di stringhe che iniziano con una vocale: %d\n", countVowelStart(words, size));
     printf("Numero di stringhe con almeno un numero: %d\n", countStringsWithNumbers(words, size));
